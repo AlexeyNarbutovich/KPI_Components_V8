5369d3884e9397d1e2baccba5e813490d7a4663b
Alexey Narbutovich <anarbutovich@gmail.com>
2021-05-16 01:56:16 +0300

    update main.js add possibility to input number of accuater from stdin.

--- Main.js
+++ Main.js
@@ -1,13 +1,23 @@
 const Tailor = require('./modules/Tailor');
 
-const iterationNumber = 4; //number is hardcoded according to requirements
+//const iterationNumber = 4; //number is hardcoded according to requirements
 
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-main(7);
+const response = function(number){
+    main(5, number);
+};
 
+readline.question("What the number of accurate: ", number => {
+    response(number);
+    readline.close();
+});
 
-function main(X){
-    const result = Tailor(X, iterationNumber);
+function main(X,N){
+    const result = Tailor(X, N);
     console.log("number is " + result);
 }
 
